9f04659314a8d6766fe83c2302960bc561c844be
mustan1653 <mustan.rampurawala@gmail.com>
2021-01-01 19:41:14 +0530

    Setup and formatting comment

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
-import {Link} from 'react-router-dom';
-
-
-const ExpenseListItem = ({ description, amount, createdAt, id}) => (
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
     <div>
-        <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
-        <p>{amount} - {createdAt}</p>
-        
+        <Link to={`/edit/${id}`}>
+            <h3>{description}</h3>
+        </Link>
+
+        <p>
+            {numeral(amount / 100).format('$0,0.00')}
+            -
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
+
     </div>
 );
 
