33a3b3cb96eb40a26a5113bec7859627526592f2
mustan1653 <mustan.rampurawala@gmail.com>
2021-01-04 10:28:10 +0530

    modified expense summary component

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -9,7 +9,7 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     const formattedExpensesTotal = numeral(expensesTotal / 10).format('$0,0.00');
     return (
         <div>
-            <h1>Viewing {expenseCount} expenses totalling {formattedExpensesTotal}</h1>
+            <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
         </div>
     );
 };
